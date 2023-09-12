b251c388b080ef6ebac6d08b2fd77478eb3c1ee8
Jhojan <jhojanrock98@gmail.com>
2023-09-12 13:20:52 -0500

    creacion de direcciones

---
+++ lib/config/menu/menu_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subtitle,
+    required this.link,
+    required this.icon});
+}
+
+ const appMenuItem = <MenuItem>[
+
+  MenuItem(
+    title: 'Botones',
+    subtitle: 'Varios Botones en Botones',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined
+  ),
+
+    MenuItem(
+    title: 'Tarjetas',
+    subtitle: 'un contenedor con muchos estilos',
+    link: '/card',
+    icon: Icons.credit_card_off_outlined
+  ),
+];
