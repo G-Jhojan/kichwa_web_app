ef25f99fc4fc25d04e561a513eb81ac1a85f17de
Jhojan <jhojanrock98@gmail.com>
2023-09-12 13:47:44 -0500

    creacion con listview.builder y list view

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kichwa_web_app/config/menu/menu_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,9 +7,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('WAR CORP APP'),
-      )
+        appBar: AppBar(
+          title: const Text('WAR CORP APP'),
+        ),
+        body: _HomeView());
+  }
+}
+
+///////////////////////////// _HomeView /////////////////////////////
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      },
+      itemCount: appMenuItems.length,
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_outlined, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      onTap: () {
+        //todo: navegar a la otra pantalla
+      },
     );
   }
 }
